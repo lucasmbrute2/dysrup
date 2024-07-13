b140083876fc990672ec311a8c33a4d2d843c6ad
lucasmbrute2 <lucasmbrute614@gmail.com>
2024-07-12 23:47:38 -0300

    feat: add project repository stub

---
+++ src/application/tests/factories/index.ts
@@ -0,0 +1,26 @@
+import { AddProjectModel } from '@/src/domain/use-cases/project/create-project'
+import { ProjectRepository } from '../../protocols/repositories/project-repository'
+import { Project } from '@/src/domain/entities/project/project'
+
+export const makeProjectModel = (): AddProjectModel => ({
+  name: 'any-name',
+  description: 'any-description',
+  started_at: new Date(),
+})
+
+export const makeProjectRepositoryStub = (): ProjectRepository => {
+  class ProjectRepositoryStub implements ProjectRepository {
+    async fetch(): Promise<Project[]> {
+      return Promise.resolve(null)
+    }
+
+    async edit(id: string, data: Project): Promise<void> {
+      return Promise.resolve(null)
+    }
+    add(project: Project): Promise<Project> {
+      return Promise.resolve(null)
+    }
+  }
+
+  return new ProjectRepositoryStub()
+}
