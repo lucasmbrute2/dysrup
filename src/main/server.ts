60a6836a7e022c52c6549847c61ad76341479f77
lucasmbrute2 <lucasmbrute614@gmail.com>
2024-07-15 19:30:24 -0300

    feat: add connection with prisma at entrypoint

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,13 +1,21 @@
+import { PrismaClient } from '@prisma/client'
 import { app } from './app'
 
-async function main() {}
+export const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.$connect()
+}
 
 main()
   .then(() => {
+    console.info("Prisma connected")
     app.listen(3333, () => {
       console.log(`Server running on port 3333`)
     })
   })
-  .catch(() => {
+  .catch(async (e) => {
+    await prisma.$disconnect()
+    console.error("Prisma was disconnected - error: " + e)
     process.exit(1)
   })
