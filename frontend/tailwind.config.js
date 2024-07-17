2d3808da86537c111f14badaf2456ae09f070a52
lucasmbrute2 <lucasmbrute614@gmail.com>
2024-07-16 23:59:23 -0300

    chore: add tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins',
+      }
+    }
+  },
+  plugins: [],
+};
